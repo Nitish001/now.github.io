e778e411e851064cf29b1e2fe6c5f9e76bccfce0
nitish prajapati <NITISH001001001@GMAIL.COM>
2017-07-02 20:32:22 +0530

    1st commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.logohover').mouseenter(function(){
+		$('.logohover').html("<p>NITISH &nbsp; KUMAR &nbsp; PRAJAPATI</p>")
+	})
+	$('.logohover').mouseleave(function(){
+		$('.logohover').html("<p>N . &nbsp; K . &nbsp; P .</p>")
+	})
+	
+});
